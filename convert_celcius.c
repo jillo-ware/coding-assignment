940ca00a84d8cad8df4d9f421b9de2c9ed92d63b
jillo-ware <doyojillo36@gmail.com>
2025-10-23 17:52:06 +0300

    Add files via upload

---
+++ convert_celcius.c
@@ -0,0 +1,26 @@
+/*
+Name:khamisi jillo doyo
+Reg No:PA106/G/28796/25
+Description:C program to convert temperature to celsius
+*/
+#include<stdio.h>
+
+float convertCelsius(float fahrenheit){
+	float celsius;
+	
+	celsius=(fahrenheit-32)*5/9;
+	
+	return celsius;
+}
+
+int main(){
+	float fahrenheit;
+	
+	printf("Enter tempereture in fahrenheit:");
+	scanf("%f", &fahrenheit);
+	
+	float celsius=convertCelsius(fahrenheit);
+	printf("The temperature in celsius is %.2f", celsius);
+		
+	return 0;
+}
