999b2ece10ee90bb88450e86605a38acf09a0f70
jillo-ware <doyojillo36@gmail.com>
2025-10-23 17:51:31 +0300

    Add files via upload

---
+++ calculate_fare.c
@@ -0,0 +1,25 @@
+/*
+Name:khamisi jillo
+Reg No:PA106/G/28796/25
+Decription:C program to compute fare
+*/
+#include <stdio.h>
+
+int calculateFare(int distance){
+	int fare;
+	fare=distance * 50;
+	
+	return fare;
+}
+
+int main(){
+	int distance;
+	
+	printf("Enter distance travelled:");
+	scanf("%d", &distance);
+	
+	int fare=calculateFare(distance);
+	printf("Your total fare is %d", fare);
+	
+	return 0;
+}
